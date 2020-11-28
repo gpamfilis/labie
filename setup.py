069ff9f5da5f126c08885008766f46f4bc341774
George Pamfilis <gpamfilis@gmail.com>
2020-11-28 23:44:15 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='labie',
+      version='0.1',
+      description='',
+      url='',
+      python_requires='>3.7',
+      author='George Pamfilis',
+      author_email='gpamfilis@gmail.com',
+      license='MIT',
+      packages=['labie'],
+      install_requires=[],
+      zip_safe=False)
